118123918d1b3937df33b4f2a320c7ea01d339e3
Awesomeguys90000 <isaacdaniels222@outlook.com>
2026-03-16 15:16:35 +1000

    Switch to per-user install (no admin required)

--- install.bat
+++ install.bat
@@ -1,7 +1,6 @@
 @echo off
 :: ============================================================
-:: Attachment PDF Converter - Installer
-:: Must be run as Administrator
+:: Attachment PDF Converter - Per-User Installer (No Admin)
 :: ============================================================
 
 echo.
@@ -10,18 +9,8 @@ echo  Attachment PDF Converter - Installer
 echo ==========================================
 echo.
 
-:: Check for admin rights
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo ERROR: This script must be run as Administrator.
-    echo Right-click the script and select "Run as administrator".
-    echo.
-    pause
-    exit /b 1
-)
-
-:: Set install directory
-set "INSTALL_DIR=%ProgramFiles%\AttachmentPdfConverter"
+:: Set install directory under user's AppData
+set "INSTALL_DIR=%LocalAppData%\AttachmentPdfConverter"
 
 :: Create install directory
 echo [1/3] Creating install directory...
@@ -35,25 +24,38 @@ copy /Y "%~dp0Microsoft.Office.Interop.Outlook.dll" "%INSTALL_DIR%\" >nul
 copy /Y "%~dp0Microsoft.Office.Interop.Word.dll" "%INSTALL_DIR%\" >nul
 copy /Y "%~dp0Microsoft.Office.Interop.Excel.dll" "%INSTALL_DIR%\" >nul
 
-:: Register the COM add-in using RegAsm
-echo [3/3] Registering COM add-in...
-set "REGASM=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe"
-if not exist "%REGASM%" set "REGASM=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe"
+:: Register COM class under HKCU (no admin needed)
+echo [3/3] Registering COM add-in (current user)...
 
-"%REGASM%" "%INSTALL_DIR%\AttachmentPdfConverter.dll" /codebase /tlb
-if %errorlevel% neq 0 (
-    echo.
-    echo ERROR: COM registration failed. Make sure .NET Framework 4.8 is installed.
-    pause
-    exit /b 1
-)
+set "CLSID={F1A2B3C4-D5E6-4F78-9A0B-C1D2E3F4A5B6}"
+set "DLL_PATH=%INSTALL_DIR%\AttachmentPdfConverter.dll"
+
+:: Register the COM class under HKCU\Software\Classes\CLSID
+reg add "HKCU\Software\Classes\CLSID\%CLSID%" /ve /d "AttachmentPdfConverter.Connect" /f >nul
+reg add "HKCU\Software\Classes\CLSID\%CLSID%\InprocServer32" /ve /d "mscoree.dll" /f >nul
+reg add "HKCU\Software\Classes\CLSID\%CLSID%\InprocServer32" /v "ThreadingModel" /d "Both" /f >nul
+reg add "HKCU\Software\Classes\CLSID\%CLSID%\InprocServer32" /v "Class" /d "AttachmentPdfConverter.Connect" /f >nul
+reg add "HKCU\Software\Classes\CLSID\%CLSID%\InprocServer32" /v "Assembly" /d "AttachmentPdfConverter, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" /f >nul
+reg add "HKCU\Software\Classes\CLSID\%CLSID%\InprocServer32" /v "RuntimeVersion" /d "v4.0.30319" /f >nul
+reg add "HKCU\Software\Classes\CLSID\%CLSID%\InprocServer32" /v "CodeBase" /d "%DLL_PATH%" /f >nul
+
+:: Register the ProgId
+reg add "HKCU\Software\Classes\AttachmentPdfConverter.Connect" /ve /d "AttachmentPdfConverter.Connect" /f >nul
+reg add "HKCU\Software\Classes\AttachmentPdfConverter.Connect\CLSID" /ve /d "%CLSID%" /f >nul
+
+:: Register as an Outlook add-in
+reg add "HKCU\Software\Microsoft\Office\Outlook\Addins\AttachmentPdfConverter.Connect" /v "FriendlyName" /d "Attachment PDF Converter" /f >nul
+reg add "HKCU\Software\Microsoft\Office\Outlook\Addins\AttachmentPdfConverter.Connect" /v "Description" /d "Converts email attachments to PDF using Microsoft Print to PDF" /f >nul
+reg add "HKCU\Software\Microsoft\Office\Outlook\Addins\AttachmentPdfConverter.Connect" /v "LoadBehavior" /t REG_DWORD /d 3 /f >nul
 
 echo.
 echo ==========================================
 echo  Installation complete!
 echo ==========================================
 echo.
-echo The add-in has been installed. Please:
+echo Installed to: %INSTALL_DIR%
+echo.
+echo Please:
 echo   1. Close Outlook if it is open
 echo   2. Re-open Outlook
 echo   3. Compose a new email to see the
--- uninstall.bat
+++ uninstall.bat
@@ -1,7 +1,6 @@
 @echo off
 :: ============================================================
-:: Attachment PDF Converter - Uninstaller
-:: Must be run as Administrator
+:: Attachment PDF Converter - Per-User Uninstaller (No Admin)
 :: ============================================================
 
 echo.
@@ -10,32 +9,20 @@ echo  Attachment PDF Converter - Uninstaller
 echo ==========================================
 echo.
 
-:: Check for admin rights
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo ERROR: This script must be run as Administrator.
-    echo Right-click the script and select "Run as administrator".
-    echo.
-    pause
-    exit /b 1
-)
+set "INSTALL_DIR=%LocalAppData%\AttachmentPdfConverter"
+set "CLSID={F1A2B3C4-D5E6-4F78-9A0B-C1D2E3F4A5B6}"
 
-set "INSTALL_DIR=%ProgramFiles%\AttachmentPdfConverter"
-
-:: Unregister the COM add-in
-echo [1/2] Unregistering COM add-in...
-set "REGASM=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe"
-if not exist "%REGASM%" set "REGASM=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe"
-
-if exist "%INSTALL_DIR%\AttachmentPdfConverter.dll" (
-    "%REGASM%" "%INSTALL_DIR%\AttachmentPdfConverter.dll" /unregister
-)
-
-:: Remove the Outlook add-in registry key (in case it wasn't cleaned up)
+:: Remove Outlook add-in registration
+echo [1/3] Removing Outlook add-in registration...
 reg delete "HKCU\Software\Microsoft\Office\Outlook\Addins\AttachmentPdfConverter.Connect" /f >nul 2>&1
 
-:: Remove install directory
-echo [2/2] Removing files...
+:: Remove COM registration
+echo [2/3] Removing COM registration...
+reg delete "HKCU\Software\Classes\CLSID\%CLSID%" /f >nul 2>&1
+reg delete "HKCU\Software\Classes\AttachmentPdfConverter.Connect" /f >nul 2>&1
+
+:: Remove installed files
+echo [3/3] Removing files...
 if exist "%INSTALL_DIR%" rmdir /s /q "%INSTALL_DIR%"
 
 echo.
